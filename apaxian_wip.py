b04219b47f7f20ef53ff7bc2340fa17875c4bc45
Jon Kartago Lamida <jon.lamida@go-jek.com>
2020-09-22 15:58:44 +0800

    Still couldn't solve apaxia

--- apaxian_wip.py
+++ apaxian_wip.py
@@ -1,7 +1,11 @@
-
-name = "rrobert"
-
-for i in range(len(name) - 1):
-    j = i + 1
-    while name[i] == name[j] and j < len(name):
-        name[i], name[j] = name[j], name[i]
+import sys
+name = next(sys.stdin).strip()
+name = [i for i in name]
+trim = 0
+for i in range(len(name)):
+    j = i+1
+    while j < len(name):
+        while name[j-1] == name[j] and j < len(name)-1:
+            name.pop(j)
+        j+=1
+print("".join(name).strip())
